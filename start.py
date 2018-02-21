4807f653e81b10e3c41f5131bf4e835c30dabeff
paulwinex <tekken3>
2018-02-21 13:27:41 +0300

    add plugins.json

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 from Qt.QtWidgets import QApplication
 # work dir should be in directory fenx.studio
 import os, sys
-sys.path.append(r'D:\work\fenx_pipeline\lib')
+# sys.path.append(r'D:\work\fenx_pipeline\lib')
 from fenx.launcher import main
 
 if __name__ == '__main__':
